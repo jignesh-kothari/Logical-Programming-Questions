893a91f81676f501384d2177aea406b5bbc9db36
jignesh-kothari <jigneshkothari467@gmail.com>
2022-05-31 19:08:07 +0530

    first commit

---
+++ AddwithoutArithmeticOperators.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+int a=7,b=5;
+while(b--) // b will take value 5,4,3,2,1,0
+a++;    // a will take values 7,8,9,10,11,12
+cout<<a;
+}
