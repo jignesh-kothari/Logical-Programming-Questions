80a9b723a20d8d5c10a55d796a8a567adb568680
Kothari Jignesh <59509659+jignesh-kothari@users.noreply.github.com>
2022-06-05 17:39:49 +0530

    c/c++ program that runs without ;

---
+++ ProgramWithoutSemicolon.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ if(printf("Programs and notes for mca!"))
+{ }
+}
+
+/*
+//Method 2 :-
+#include <stdio.h>
+#include <conio.h>
+
+#define m ;
+void main()
+{
+ printf("Programs and notes for mca")m   //m as semicolon
+}*/
