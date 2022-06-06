acf085d26514beb59f86cd2aceddfb8562ac3101
Kothari Jignesh <59509659+jignesh-kothari@users.noreply.github.com>
2022-06-06 09:05:46 +0530

    left half piramid

---
+++ Left_half_pitamid_reverse_full_star.cpp
@@ -0,0 +1,27 @@
+/*
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        * 
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, k;
+    for(i=5;i>=1;i--)
+    {
+        for(j=5;j>i;j--)
+        {
+        	cout << " ";
+        }
+        for(k=1;k<=i;k++)
+        {
+        	cout << "*";            
+        }
+        cout <<endl;        
+    }
+    return 0;
+}
