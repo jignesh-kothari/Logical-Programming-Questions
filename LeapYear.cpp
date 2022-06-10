53fdcbb2ba9077a94ecc06f74a02d9c5ec52b875
Kothari Jignesh <59509659+jignesh-kothari@users.noreply.github.com>
2022-06-10 18:58:05 +0530

    Add files via upload

---
+++ LeapYear.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int year;
+  cout << "Enter Number : ";
+  cin >> year;
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0)
+        cout << year << " is a leap year";
+      else
+        cout << year << " is not a leap year";
+    } else
+      cout << year << " is a leap year";
+  } else
+    cout << year << " is not a leap year";
+  return 0;
+}
