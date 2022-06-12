a4e92c76e0bf0e92be7fa71753f029e3db52d95f
Kothari Jignesh <59509659+jignesh-kothari@users.noreply.github.com>
2022-06-12 16:52:24 +0530

    reverse number

---
+++ print_number_in_reverse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,r,sum=0;
+    cout<<"Enter Number :
+    cin>>n;
+    while(n!=0)
+    {
+        r=n%10;
+        sum=sum*10+r;
+        n=n/10;
+    }
+    cout<<"Reverse is : "<<sum;
+	return 0;
+}
