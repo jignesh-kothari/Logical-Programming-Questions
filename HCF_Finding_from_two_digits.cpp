b254941a091be2383b2e198184705109a91fb26e
Kothari Jignesh <59509659+jignesh-kothari@users.noreply.github.com>
2022-06-13 17:40:03 +0530

    HCF

---
+++ HCF_Finding_from_two_digits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b, hcf, r;
+  cout << "Enter two numbers : ";
+  cin >>a>>b;
+  while (true) {
+    hcf = b;
+    r = a % b;
+    a = b;
+    b = r;
+    if (b == 0) {
+      break;
+    }
+  }
+  cout << "HCF is " << hcf;
+}
