942c6727655431068033544f020e5e4099f086dc
Kothari Jignesh <59509659+jignesh-kothari@users.noreply.github.com>
2022-06-21 08:24:53 +0530

    add withou plus operatar

---
+++ Add_Without_Plus_operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a,b,c;
+    cout<<"Enter Value For A: ";
+    cin>>a;
+    cout<<endl<<"Enter Value For B: ";
+    cin>>b;
+    c=a-(-b);
+    cout<<endl<<"Ans is : "<<c;
+    
+    return 0;
+}
