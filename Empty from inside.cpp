2e27b7aa341bc17443cd21f6954947a037a5dd32
Kothari Jignesh <59509659+jignesh-kothari@users.noreply.github.com>
2022-06-23 09:40:43 +0530

    empty piramid from inside

---
+++ Empty from inside.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n,i,j;
+    cout<<"Enter Number: ";
+    cin>>n;
+    
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(j==i || j==1 || i==n)
+                cout<<"*";
+            else
+                cout<<" ";
+        }    
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+/*
+output
+
+Enter Number: 10
+*
+**
+* *
+*  *
+*   *
+*    *
+*     *
+*      *
+*       *
+**********
+
+*/
